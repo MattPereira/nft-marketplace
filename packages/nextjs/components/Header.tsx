b3e600a6114cf50608c070dde8090b6593381263
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-09-29 17:04:21 -0700

    update webhook route path

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -90,7 +90,7 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Nft Marketplace</span>
+            <span className="font-bold leading-tight">NFT Marketplace</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
